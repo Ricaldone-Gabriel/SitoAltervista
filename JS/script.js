a1346e7ec7e9d2f5f8f564edf3698a7bddbab009
Ricaldone-Gabriel <gabriel.ricaldone@jcmaxwell.it>
2021-11-18 13:35:46 +0100

    Pulsanti articolo 1

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,16 @@
 //ESTRELLA PLATINADA EL MUNDOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+function tabellina1() {
+    alert("2 4 6 8 10 12 18 20");
+}
+function tabellina2() {
+    alert("3 6 9 12 15 18 21 24 27 30");
+}
+function tabellina3() {
+    alert("4 8 12 16 20 24 28 32 36 40")
+}
+function tabellina4() {
+    alert("5 10 15 20 25 30 35 40 45 50")
+}
+function tabellina5() {
+    alert("6 12 18 24 30 36 42 48 54 60");
+}
