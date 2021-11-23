cdeade2d95e5de64d0cbdf3185ef031670defa9a
Ricaldone-Gabriel <gabriel.ricaldone@jcmaxwell.it>
2021-11-23 18:41:40 +0100

    Fixato i punti e virgola

--- JS/script.js
+++ JS/script.js
@@ -10,11 +10,11 @@ function tabellina2() {
 }
 function tabellina3() {
     document.getElementById("tabellina").innerHTML=  "4 8 12 16 20 24 28 32 36 40";
-    alert("4 8 12 16 20 24 28 32 36 40")
+    alert("4 8 12 16 20 24 28 32 36 40");
 }
 function tabellina4() {
     document.getElementById("tabellina").innerHTML=  "5 10 15 20 25 30 35 40 45 50";
-    alert("5 10 15 20 25 30 35 40 45 50")
+    alert("5 10 15 20 25 30 35 40 45 50");
 }
 function tabellina5() {
     document.getElementById("tabellina").innerHTML=  "6 12 18 24 30 36 42 48 54 60";
