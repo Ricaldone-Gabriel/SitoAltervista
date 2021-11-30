a0708bce1180977f13846626389e3c8171e61ee4
Ricaldone-Gabriel <gabriel.ricaldone@jcmaxwell.it>
2021-11-30 18:54:44 +0100

    JavaScript 2

---
+++ JS/script2.js
@@ -0,0 +1,16 @@
+//ESTRELLA PLATINADA EL MUNDOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+function immagine1() {
+    document.getElementById("immagine7").src=  "../../Immagini/Javascript2/and.jpg";
+}
+function immagine2() {
+    document.getElementById("immagine7").src=  "../../Immagini/Javascript2/or.jpg";
+}
+function immagine3() {
+    document.getElementById("immagine7").src=  "../../Immagini/Javascript2/not.jpg";
+}
+function immagine4() {
+    document.getElementById("immagine7").src=  "../../Immagini/Javascript2/nand.jpg";
+}
+function immagine5() {
+    document.getElementById("immagine7").src=  "../../Immagini/Javascript2/nor.jpg";
+}
