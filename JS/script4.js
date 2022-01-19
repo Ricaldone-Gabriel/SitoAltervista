335f2c2974631612b99bfd52f3753d4c1f495fb8
Ricaldone-Gabriel <gabriel.ricaldone@jcmaxwell.it>
2022-01-19 17:46:59 +0100

    Bottoni

--- JS/script4.js
+++ JS/script4.js
@@ -1,3 +1,12 @@
+function cambiacolore1() {
+    document.getElementById("boxColore").style.backgroundColor = "red";
+}
 function cambiacolore2() {
-    document.getElementById("box2").style.backgroundColor = "green";
+    document.getElementById("boxColore").style.backgroundColor = "green";
+}
+function cambiacolore3() {
+    document.getElementById("boxColore").style.backgroundColor = "blue";
+}
+function cambiacolore4() {
+    document.getElementById("boxColore").style.backgroundColor = "yellow";
 }
